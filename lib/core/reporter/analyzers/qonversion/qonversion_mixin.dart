a880d01fd1143e0e164ba39112f8bad6b38f9c27
Caner Gülay <caner.gulay@hotmail.com>
2021-10-12 16:27:06 +0300

    analyzers was improved

--- lib/core/reporter/analyzers/qonversion/qonversion_mixin.dart
+++ lib/core/reporter/analyzers/qonversion/qonversion_mixin.dart
@@ -3,11 +3,9 @@ import 'package:unirepo/magic_box.dart';
 
 mixin QonversionMixin {
   Future<void> initQonversion() async {
-    final QLaunchResult launchResult = await Qonversion.launch(
+    await Qonversion.launch(
       MagicBox.shared.qonversionProjectKey,
       isObserveMode: false,
     );
   }
-
-  Future<void> qonversionsLogBottomBarEvent({required Map<String, dynamic> data}) async {}
 }
