fbe079e3618f5af3bfa2de69c33863725d2ab165
canergulay <caner@dasas.co>
2021-10-17 14:06:38 +0300

    fup

--- lib/features/landing/widgets/bottom_navigation_bar/bottom_navigation_provider.dart
+++ lib/features/landing/widgets/bottom_navigation_bar/bottom_navigation_provider.dart
@@ -4,20 +4,22 @@ class BottomNavigationProvider extends ChangeNotifier {
   int bottomNavigationState = 0;
 
   void changePage(int page) {
+    print(page);
     bottomNavigationState = page;
     notifyListeners();
     _reportBottomNavigaton(page);
   }
 
   void _reportBottomNavigaton(int index) {
-    // HERE WE ARE LOGGING WHAT BOTTOM NAVIGATION BAR BUTTON USER PRESSES
-    // EITHER PROFILE, OR HOMEPAGE.
-    // WE CAN TRACK THIS DATA HERE
     late final String buttonValue;
     if (index == 1) {
       buttonValue = 'profile';
     } else {
       buttonValue = 'home_page';
     }
+<<<<<<< HEAD:lib/features/landing/widgets/bottom_navigation_bar/bottom_navigation_provider.dart
+=======
+    ReportManager.instance.logBottomNavbarEventCollective(data: {'pressed_button': buttonValue});
+>>>>>>> parent of 4d0ebbd... info added:lib/features/landing/widgets/bottom_navigation_bar/bototm_navigation_provider.dart
   }
 }
